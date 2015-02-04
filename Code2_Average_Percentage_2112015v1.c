b3dc25d3ba6df6ca2644a19ba976aa6b09d6ff6e
Shubham Varshney <varshneyshubham98@gmail.com>
2015-02-04 22:19:15 +0530

    Update and rename Code2_Average_Percentage_2112015v1 to Code2_Average_Percentage_2112015v1.c

--- Code2_Average_Percentage_2112015v1.c
+++ Code2_Average_Percentage_2112015v1.c
@@ -17,7 +17,17 @@ void main () {
 	scanf("%f",&SST);
 	average=(maths+science+hindi+english+SST)/5;
 	printf("The average of the above marks is: %f",average);
-	printf("\nDo you want to know your percentage? Y/N :");
-	scanf("%s",&answer);
+	printf("\nInput the maximum marks in maths:");
+	scanf("%f",&maxMaths);
+	printf("Input the maximum marks in science:");
+	scanf("%f",&maxScience);
+	printf("Input the maximum marks in hindi:");
+	scanf("%f",&maxHindi);
+	printf("Input the maximum marks in english:");
+	scanf("%f",&maxEnglish);
+	printf("Input the maximum marks in SST:");
+	scanf("%f",&maxSST);
+	percentage=((average*500)/(maxMaths+maxScience+maxHindi+maxEnglish+maxSST));
+	printf("The percentage is:%f",percentage);
 	getch();
 }
