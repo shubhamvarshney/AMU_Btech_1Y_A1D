cb3f9aade26e6f1aaade043138c9418c2e40328c
Shubham Varshney <varshneyshubham98@gmail.com>
2015-02-04 22:41:11 +0530

    Update Code5_SumOfDigits_FiveDigitNumber_4212015.c

--- Code5_SumOfDigits_FiveDigitNumber_4212015.c
+++ Code5_SumOfDigits_FiveDigitNumber_4212015.c
@@ -3,7 +3,7 @@
 void main () {
 	clrscr();
 	int number,digit1,digit2,digit3,digit4,digit5,sumOfDigits;
-	printf("This program will find the sum of digits of a input number that can be upto five digits");
+	printf("This program will find the sum of digits of a input number that can be upto five digits\n");
 	printf("Input the number:");
 	scanf("%d",&number);
 	digit5=number%10;
